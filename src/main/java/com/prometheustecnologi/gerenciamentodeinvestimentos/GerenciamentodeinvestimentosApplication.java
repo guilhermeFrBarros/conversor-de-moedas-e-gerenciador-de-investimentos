9efd25b0dc2fbfb71e60a7f3c6b4ad85cb8048f0
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-07-25 10:35:26 -0300

    first commit

---
+++ src/main/java/com/prometheustecnologi/gerenciamentodeinvestimentos/GerenciamentodeinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.prometheustecnologi.gerenciamentodeinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentodeinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentodeinvestimentosApplication.class, args);
+	}
+
+}
