fb5823ccfe5001d1ce315664ed1ec1a66ca27aee
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2024-04-22 15:20:38 -0300

    feat: configuration cors

--- src/main/java/com/prometheustecnologi/gerenciamentodeinvestimentos/config/CorsConfiguration.java
+++ src/main/java/com/prometheustecnologi/gerenciamentodeinvestimentos/config/CorsConfiguration.java
@@ -1,8 +1,11 @@
 package com.prometheustecnologi.gerenciamentodeinvestimentos.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
